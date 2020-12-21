dc304499a06d76482936cbbb456673b55dabb032
Kahvi Patel <iamkahvi@gmail.com>
2020-12-21 14:57:28 -0500

    KP: Adding sliding window function for pt2 and comments for functions

--- day09/9.js
+++ day09/9.js
@@ -6,6 +6,7 @@ var fs_1 = require("fs");
 // After that, each number you receive should be the sum of any two of the 25
 // immediately previous numbers. The two numbers will have different values,
 // and there might be more than one such pair.
+var LENGTH = 25;
 function main() {
     var lines;
     try {
@@ -21,13 +22,47 @@ function main() {
         var l = lines_1[_i];
         numbers.push(+l);
     }
-    for (var i = 0; i + 25 < numbers.length; i++) {
-        if (!sumExists(numbers.slice(i, i + 25), numbers[i + 25])) {
-            console.log(numbers[i + 25]);
+    var invalidNum = 0;
+    for (var i = 0; i + LENGTH < numbers.length; i++) {
+        if (!sumExists(numbers.slice(i, i + LENGTH), numbers[i + LENGTH])) {
+            invalidNum = numbers[i + LENGTH];
             break;
         }
     }
+    var _a = slidingWindowSum(numbers, invalidNum), start = _a[0], end = _a[1];
+    var max = 0;
+    var min = Number.MAX_SAFE_INTEGER;
+    for (var _b = 0, _c = numbers.slice(start, end); _b < _c.length; _b++) {
+        var n = _c[_b];
+        if (n > max) {
+            max = n;
+        }
+        if (n < min) {
+            min = n;
+        }
+    }
+    console.log(max + min);
 }
+// Find indexes of subarray that sums to the number provided, or return [0,0]
+function slidingWindowSum(arr, desiredSum) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum = 0;
+        var j = i;
+        while (sum < desiredSum) {
+            sum += arr[j];
+            if (sum === desiredSum) {
+                return [i, j + 1];
+            }
+            if (sum > desiredSum) {
+                break;
+            }
+            j++;
+        }
+    }
+    return [0, 0];
+}
+// Return true if any two numbers in the array sum to the number provided
 function sumExists(arr, sum) {
     var m = {};
     for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
--- day09/9.ts
+++ day09/9.ts
@@ -6,6 +6,8 @@ import { readFileSync } from "fs";
 // immediately previous numbers. The two numbers will have different values,
 // and there might be more than one such pair.
 
+const LENGTH = 25;
+
 function main() {
   let lines: string[];
 
@@ -23,14 +25,52 @@ function main() {
     numbers.push(+l);
   }
 
-  for (let i = 0; i + 25 < numbers.length; i++) {
-    if (!sumExists(numbers.slice(i, i + 25), numbers[i + 25])) {
-      console.log(numbers[i + 25]);
+  let invalidNum = 0;
+  for (let i = 0; i + LENGTH < numbers.length; i++) {
+    if (!sumExists(numbers.slice(i, i + LENGTH), numbers[i + LENGTH])) {
+      invalidNum = numbers[i + LENGTH];
       break;
     }
   }
+
+  const [start, end] = slidingWindowSum(numbers, invalidNum);
+
+  let max = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+  for (const n of numbers.slice(start, end)) {
+    if (n > max) {
+      max = n;
+    }
+    if (n < min) {
+      min = n;
+    }
+  }
+  console.log(max + min);
 }
 
+// Find indexes of subarray that sums to the number provided, or return [0,0]
+function slidingWindowSum(arr: number[], desiredSum: number): [number, number] {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = 0;
+    let j = i;
+
+    while (sum < desiredSum) {
+      sum += arr[j];
+      if (sum === desiredSum) {
+        return [i, j + 1];
+      }
+      if (sum > desiredSum) {
+        break;
+      }
+      j++;
+    }
+  }
+  return [0, 0];
+}
+
+// Return true if any two numbers in the array sum to the number provided
 function sumExists(arr: number[], sum: number): boolean {
   let m: { [key: string]: boolean } = {};
   for (const num of arr) {
